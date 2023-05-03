286cec827143bcfd03ef85d57f0f815545bfddf7
Root <chanrick784@gmail.com>
2023-05-03 14:27:17 +0800

    finish lab3

---
+++ lab3/hash.h
@@ -0,0 +1,27 @@
+#ifndef __HASH_H__
+#define __HASH_H__
+
+#include "table.h"
+#include "list.h"
+#define HASHSLOT_SIZE 0x3fff
+#define MAX_NESTED_DEPTH 10
+
+typedef struct hashtable_t
+{
+    listnode_t *hashslot[HASHSLOT_SIZE];
+    listnode_t *stack[MAX_NESTED_DEPTH];
+    int stack_frame;
+} hashtable_t;
+
+void hash_init(hashtable_t *hashtable);
+
+tnode_t *hash_search(hashtable_t *hashtable, char *name, ttype type);
+
+int hash_insert(hashtable_t *hashtable, tnode_t *tnode);
+
+void hash_stack_increase(hashtable_t *hashtable);
+
+void hash_stack_decrease(hashtable_t *hashtable);
+
+extern hashtable_t symbol_hashtable;
+#endif
