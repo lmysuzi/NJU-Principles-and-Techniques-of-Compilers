82f43a7fdd8f0ad5bd6c31727c924a7a688f4dee
Root <chanrick784@gmail.com>
2023-05-03 14:38:57 +0800

    delete

--- lab3/errorhandle.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ERRORHANDLE_H__
-#define __ERRORHANDLE_H__
-
-#include <stdio.h>
-
-void throw_error(int error_code, int line, char *object_name);
-
-#endif
--- lab3/make.sh
+++
@@ -1,7 +0,0 @@
-flex lexical.l
-bison -d syntax.y
-gcc translate.c errorhandle.c list.c hash.c table.c syntax.tab.c tree.c main.c -lfl -ly -o ./parser
-
-./parser Test/selftest.cmm out1.ir
-echo 李明扬别卷了快点回来搞枪战啊！！！！！！！！！别勾八写了啊明天再搞
-echo 速速打枪速速打枪！速速打枪速速打枪！速速打枪速速打枪！速速打枪速速打枪！速速打枪速速打枪！
